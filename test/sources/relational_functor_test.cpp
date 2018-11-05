9277508c3670a23de23805f1270a4e465a7faf64
Michael Heilmann <michaelheilmann@primordialmachine.com>
2018-11-05 04:29:19 +0100

    Initial commit

---
+++ test/sources/relational_functor_test.cpp
@@ -0,0 +1,20 @@
+#include "primordialmachine/relational_functors/include.hpp"
+#include "gtest/gtest.h"
+
+TEST(relational_functors_test, lower_than_functor_test)
+{ /* Intentionally empty. */ }
+
+TEST(relational_functors_test, lower_than_or_equal_to_functor_test)
+{ /* Intentionally empty. */ }
+
+TEST(relational_functors_test, greater_than_functor_test)
+{ /* Intentionally empty. */ }
+
+TEST(relational_functors_test, greater_than_or_equal_to_functor_test)
+{ /* Intentionally empty. */ }
+
+TEST(relational_functors_test, equal_to_functor_test)
+{ /* Intentionally empty. */ }
+
+TEST(relational_functors_test, not_equal_to_functor_test)
+{ /* Intentionally empty. */ }
